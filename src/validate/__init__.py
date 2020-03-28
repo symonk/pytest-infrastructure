057bab6339c9df14f5f8e77d8c048ea3e24b9ad9
symonk <jackofspaces@gmail.com>
2020-03-28 17:41:47 +0000

    put stdout sink back in

--- src/validate/__init__.py
+++ src/validate/__init__.py
@@ -1,8 +1,10 @@
+from sys import stdout
 from validate._version import version as __version__
 from validate.decorators import validate
 from loguru import logger
 
 logger.add(
+    stdout,
     colorize=True, enqueue=True,
 )
 new_level = logger.level("functions", no=38, color="<yellow>", icon="🐍")
