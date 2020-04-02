65a8cedaf2c32b8e8c646fb8e6f5f7608aca37ae
symonk <jackofspaces@gmail.com>
2020-04-02 01:34:18 +0100

    test negative scenario where file path cannot be found

---
+++ testing/unit/test_testing_utils.py
@@ -0,0 +1,8 @@
+import pytest
+
+from testing.testing_utils import get_path_to_test_file
+
+
+def test_sample_file_no_file():
+    with pytest.raises(FileNotFoundError):
+        get_path_to_test_file("fail.py")
