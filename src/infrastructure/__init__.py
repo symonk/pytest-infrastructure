85951c0f8ca50c08ebb7dce36265a7c8e21f611d
symonk <jackofspaces@gmail.com>
2020-04-12 00:53:34 +0100

    [master]:rocket: remove dead code

--- src/infrastructure/__init__.py
+++ src/infrastructure/__init__.py
@@ -3,9 +3,6 @@ import os
 
 from loguru import logger
 from .strings import INFRASTRUCTURE_PLUGIN_NAME
-from .function_finder import InfrastructureFunctionFinder  # noqa
-from .function_manager import FunctionManager  # noqa
-from .function_scheduler import FunctionScheduler  # noqa
 
 config = {
     "handlers": [
