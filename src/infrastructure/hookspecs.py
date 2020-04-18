1969a6b263c2d6da6fe0dd91b8c49a95450b5d5d
symonk <jackofspaces@gmail.com>
2020-04-18 18:47:05 +0100

    [master]:rocket: starting to implement custom hooks

---
+++ src/infrastructure/hookspecs.py
@@ -0,0 +1,15 @@
+from pluggy import HookspecMarker, HookimplMarker
+
+hookspec = HookspecMarker("pytest-infra")
+hookimpl = HookimplMarker("pytest-infra")
+
+
+class InfrastructureHooks:
+
+    @hookspec
+    def collect_functions(self):
+         """ collect validation functions """
+
+    @hookspec
+    def validate(self):
+        """ validate the functions """
