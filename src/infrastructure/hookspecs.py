c39f240e43d29057e023c11619850ddf27bfd45d
symonk <jackofspaces@gmail.com>
2020-11-03 17:05:27 +0000

    make collect hook firstresult only

--- src/infrastructure/hookspecs.py
+++ src/infrastructure/hookspecs.py
@@ -9,9 +9,10 @@ hookimpl = HookimplMarker("pytest")
 
 
 class InfrastructureHookSpecs:
-    @hookspec()
+    @hookspec(firstresult=True)
     def pytest_infrastructure_collect(self) -> List[Callable]:
-        """ Hook for performing the collection of infrastructure functions """
+        """ Hook for performing the collection of infrastructure functions.
+            Implement your own hook and return the List[Callable] functions which adhere to what you desire. """
 
     @hookspec()
     def pytest_infrastructure_validate(self, functions: List[Callable]):
